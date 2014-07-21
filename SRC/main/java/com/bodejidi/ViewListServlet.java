21cf2283710b74b6a2bcee9c874ac321c83db300
yuanchangjun <qinxiangdexunyicao@gmail.com>
2014-07-21 15:13:28 +0800

    增加了简单的数据库连接代码

--- SRC/main/java/com/bodejidi/ViewListServlet.java
+++ SRC/main/java/com/bodejidi/ViewListServlet.java
@@ -6,12 +6,26 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
 
+import java.sql.SQLException;
+import java.sql.DriverManager;
+import java.sql.Connection;
+
 public class ViewListServlet extends HttpServlet{
     public void doGet(HttpServletRequest req,HttpServletResponse resp)
         throws IOException,ServletException{
-            resp.getWriter().println("test Servlet!");
+           
+        String uri = "jdbc:mysql://localhost/test?username=root&password=";
+       try{ 
+           Class.forName("com.mysql.jdbc.Driver");
+
+           Connection connection = DriverManager.getConnection(uri);
+                
+       }catch(Exception e){
+
+        System.out.println(e);
+
+       }
+
 
     }
-
-
 }
